5c0967daf418f12db0701deef51e49ae16c383cf
Tomas Aparicio <tomas@aparicio.me>
2014-09-27 10:52:40 +0200

    chore(version): bump

--- src/all.js
+++ src/all.js
@@ -1,7 +1,7 @@
 (function (global) {
   var lil = global.lil = global.lil || {}
 
-  lil.VERSION = '0.1.5'
+  lil.VERSION = '0.1.6'
   lil.alias = lil.globalize = function () {
     global._ = lil
   }
